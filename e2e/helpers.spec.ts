a09cb31605ef489614543757c33f7a4a8466345d
Vanessa Kelly <vkelly@cannabiz.media>
2022-09-06 19:03:00 -0400

    merging extension profile

--- e2e/helpers.spec.ts
+++ e2e/helpers.spec.ts
@@ -1,7 +1,55 @@
-import { test, expect } from '@playwright/test';
+import { Browser, BrowserContext, test, expect } from '@playwright/test';
 
 export async function delay(ms: number) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+export async function metamask(){
 
+const { test: base, browser, chromium, webkit } = require('@playwright/test')
+const path = require('path')
+
+const extensionPath = path.join(__dirname, '../metamask') // make sure this is correct
+
+const test = base.extend({
+  context: async ({ browserName }, use) => {
+    const browserTypes = { chromium, webkit }
+    const launchOptions = {
+      devtools: true,
+      headless: false,
+      args: [
+       `--disable-extensions-except=${extensionPath}`,
+       `--load-extension=${extensionPath}`
+      ],
+      viewport: {
+        width: 1000,
+        height: 800
+      }
+    }
+    const context = await browserTypes[browserName].launchPersistentContext(
+      '/tmp/test-user-data-dir',
+      launchOptions
+    )
+    await use(context)
+    await context.close()
+  }
+});
+}
+export async function login(ms: number) {
+  test('MetaMask Extension Login', async ({ page }) => {
+    let browser: Browser;
+    let context: BrowserContext;
+    //let page: Page;
+    await delay(2000);
+    await page.reload();
+    await page.goto(
+     'chrome-extension://daackfnalkpkoipabdoioillppgeekja/popup.html');  
+  
+    //await page.keyboard.press('Control+Tab');
+    //await page.pause();
+   // await page.waitForTimeout(30000); // this is here so that it won't automatically close the browser window
+await page.locator('input[type="password"]').click();
+await page.locator('input[type="password"]').fill('Aut0mati0n_2022!');
+await page.locator('button:has-text("Unlock")').click();
+});
+}
