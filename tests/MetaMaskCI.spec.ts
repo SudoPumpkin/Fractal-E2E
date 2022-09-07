5d38dd717b59ef4f5d6a659e6d82771861ee4b27
Vanessa Kelly <vkelly@cannabiz.media>
2022-09-07 14:15:58 -0400

    updated metamask login/connect test

--- tests/MetaMaskCI.spec.ts
+++ tests/MetaMaskCI.spec.ts
@@ -14,7 +14,7 @@ const test = base.extend({
       headless: false,
       args: [
        `--disable-extensions-except=${extensionPath}`,
-       //`--load-extension=${extensionPath}`
+       `--load-extension=${extensionPath}`
       ],
       viewport: {
         width: 1000,
@@ -27,7 +27,7 @@ const test = base.extend({
     )
     await use(context)
     await context.close()
-  },
+  }
 });
 
 test.describe('Connect Wallet', () => {
@@ -37,108 +37,67 @@ test.describe('Connect Wallet', () => {
      const page = await context.newPage();
       await delay(5000);
       await page.reload();
-      // << new
-      await page.goto('chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/popup.html');
-      await page.click('.button btn--rounded btn-primary first-time-flow__button');
-     // await page.keyboard.press('Control+3');
-      //await page.goto('chrome-extension://daackfnalkpkoipabdoioillppgeekja/home.html#initialize/select-action');
-     // await page.reload();
+      //await page.keyboard.press('Control+Tab');
+      await page.goto('chrome-extension://daackfnalkpkoipabdoioillppgeekja/home.html#initialize/select-action');
+      await page.reload();
       // Click text=Import wallet
-     // await page.locator('text=Import wallet').click();
+      await page.locator('text=Import wallet').click();
       await page.waitForURL('chrome-extension://daackfnalkpkoipabdoioillppgeekja/home.html#initialize/metametrics-opt-in');
       //await page.pause();
       await page.reload();
-      await page.click('text=I Agree');
-      await page.reload();
-      await page.locator('text=Import wallet').click();
       // Click [data-testid="page-container-footer-next"]
-     // await page.locator('[data-testid="page-container-footer-next"]').click();
+      await page.locator('[data-testid="page-container-footer-next"]').click();
       await page.waitForURL('chrome-extension://daackfnalkpkoipabdoioillppgeekja/home.html#initialize/create-password/import-with-seed-phrase');
 
         // Click [placeholder="Paste Secret Recovery Phrase from clipboard"]
-  await page.locator('[placeholder="Paste Secret Recovery Phrase from clipboard"]').click();
-  // Fill [placeholder="Paste Secret Recovery Phrase from clipboard"]
-  await page.locator('[placeholder="Paste Secret Recovery Phrase from clipboard"]').fill('liar input bronze remember clean harsh imitate bulb anchor sight park fade');
-  // Click #password
-  await page.locator('#password').click();
-  // Fill #password
-  await page.locator('#password').fill('FractalT35t_2022!');
-  // Click #confirm-password
-  await page.locator('#confirm-password').click();
-  // Fill #confirm-password
-  await page.locator('#confirm-password').fill('FractalT35t_2022!');
-  // Click div[role="checkbox"] >> nth=1
-  await page.locator('div[role="checkbox"]').nth(1).click();
-  // Click button:has-text("Import")
-  await page.locator('button:has-text("Import")').click();
-  await page.waitForURL('chrome-extension://daackfnalkpkoipabdoioillppgeekja/home.html#initialize/end-of-flow');
-  // assert text=Congratulations
-  await page.locator('text=Congratulations');
-  // Click text=All Done
-  await page.locator('text=All Done').click();
-  // Go to Fractal dev
-  await page.goto('https://app.dev.fractalframework.xyz/');
-  //await page.pause();
+    await page.locator('[placeholder="Paste Secret Recovery Phrase from clipboard"]').click();
+    // Fill [placeholder="Paste Secret Recovery Phrase from clipboard"]
+    await page.locator('[placeholder="Paste Secret Recovery Phrase from clipboard"]').fill('liar input bronze remember clean harsh imitate bulb anchor sight park fade');
+    // Click #password
+    await page.locator('#password').click();
+    // Fill #password
+    await page.locator('#password').fill('FractalT35t_2022!');
+    // Click #confirm-password
+    await page.locator('#confirm-password').click();
+    // Fill #confirm-password
+    await page.locator('#confirm-password').fill('FractalT35t_2022!');
+    // Click div[role="checkbox"] >> nth=1
+    await page.locator('div[role="checkbox"]').nth(1).click();
+    // Click button:has-text("Import")
+    await page.locator('button:has-text("Import")').click();
+    await page.waitForURL('chrome-extension://daackfnalkpkoipabdoioillppgeekja/home.html#initialize/end-of-flow');
+    // assert text=Congratulations
+    await page.locator('text=Congratulations');
+    // Click text=All Done
+    await page.locator('text=All Done').click();
+    await page.reload();
+    // Click [data-testid="popover-close"]
+    await page.locator('[data-testid="popover-close"]').click();
+    // Click div[role="button"]:has-text("Ethereum Mainnet")
+    await page.locator('div[role="button"]:has-text("Ethereum Mainnet")').click();
+    // Click text=✓Goerli Test Network
+    await page.locator('text=✓Goerli Test Network').click();
+    
+    // Go to Fractal dev
+    await page.goto('https://app.dev.fractalframework.xyz/');
+    //await page.pause();
 
-  // Click button:has-text("Connect Wallet")
-  await page.locator('button:has-text("Connect Wallet")').click();
-  // Click button[role="menuitem"]:has-text("Connect Wallet")
-  await page.locator('button[role="menuitem"]:has-text("Connect Wallet")').click();
-  // Follows MetaMask popup window
-  const [popup] = await Promise.all([
-    context.waitForEvent('page'),
-    page.locator('text=MetaMaskConnect to your MetaMask Wallet').click()
-  ]);
-  //await page.pause();
-   // Click text=Next
-   await popup.locator('text=Next').click();
+    // Click button:has-text("Connect Wallet")
+    await page.locator('button:has-text("Connect Wallet")').click();
+    // Click button[role="menuitem"]:has-text("Connect Wallet")
+    await page.locator('button[role="menuitem"]:has-text("Connect Wallet")').click();
+    // Follows MetaMask popup window
+    const [popup] = await Promise.all([
+      context.waitForEvent('page'),
+      page.locator('text=MetaMaskConnect to your MetaMask Wallet').click()
+    ]);
+    //await page.pause();
+    // Click text=Next
+    await popup.locator('text=Next').click();
 
-   //await page.pause();
-   // Click 'Connect' - I know it's an ugly selector but it's hidden deep within the html =/
-   await popup.click('//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div[2]/footer/button[2]');
-   //await newPage.locator('text=Connect').click();
-   //await page.pause();
-  //button[role="menuitem"]:has-text("Connect Wallet
- // await page.pause();
-  // Go to another tab
-  // something
-  //await page.keyboard.press('Control+Tab');
-  //const context = await browser.newContext();
-// const page4 = await context.newPage();
-//   await page4.goto('chrome-extension://daackfnalkpkoipabdoioillppgeekja/home.html#connect/192068ca-982d-4d76-95c3-4eaad7109638/confirm-permissions');
-//   await page.pause();
-//   // Click text=Next
-//   await page.locator('text=Next').click();
-//   await expect(page).toHaveURL('chrome-extension://daackfnalkpkoipabdoioillppgeekja/home.html#connect/192068ca-982d-4d76-95c3-4eaad7109638/confirm-permissions')
-  // Open new page
-// const context = await browser.newContext();
-// const page4 = await context.newPage();
-// await page4.goto('chrome-extension://daackfnalkpkoipabdoioillppgeekja/notification.html');
-// // Click text=MetaMaskConnect to your MetaMask Wallet
-// await page.locator('text=MetaMaskConnect to your MetaMask Wallet').click();
-// await page.waitForURL('chrome-extension://daackfnalkpkoipabdoioillppgeekja/notification.html#connect/5b31749a-0103-4698-a804-9b2d919b3057');
-// // Go to chrome-extension://daackfnalkpkoipabdoioillppgeekja/notification.html#connect/5b31749a-0103-4698-a804-9b2d919b3057/confirm-permissions
-// await page4.goto('chrome-extension://daackfnalkpkoipabdoioillppgeekja/notification.html#connect/5b31749a-0103-4698-a804-9b2d919b3057/confirm-permissions');
-// // Go to chrome-extension://daackfnalkpkoipabdoioillppgeekja/notification.html#
-// await page4.goto('chrome-extension://daackfnalkpkoipabdoioillppgeekja/notification.html#');
-// // Close page
-// await page4.close();
-// // Click #switchChain
-// await page.locator('#switchChain').click();
-  
-  // Note that Promise.all prevents a race condition
-// between clicking and waiting for the popup.
-// const [popup] = await Promise.all([
-//   // It is important to call waitForEvent before click to set up waiting.
-//   page.waitForEvent('popup'),
-//   // Click text=MetaMaskConnect to your MetaMask Wallet
-//   page.locator('text=MetaMaskConnect to your MetaMask Wallet').click()]);
-//   await popup.waitForLoadState();
-//   console.log(await popup.title());
+    //await page.pause();
+    // Click 'Connect' - I know it's an ugly selector but it's hidden deep within the html =/
+    await popup.click('//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div[2]/footer/button[2]');
 
-  //const confirm = page.locator('//div[@class="Toastify"]');
-
-  //await page.reload;
- //await page.pause();
     });
 });
